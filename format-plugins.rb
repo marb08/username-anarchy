6ece4b07dd6fb26a6cf70fc79870221ecf987518
jmv <jac.valtorta@gmail.com>
2023-06-23 15:49:18 +0200

    Update format-plugins.rb with more usecase, like - and _

--- format-plugins.rb
+++ format-plugins.rb
@@ -1,212 +1,305 @@
 # Copyright
-# Part of Username Anarchy
+# Part of Username Anarchy with marb08 contributions
 
 class Plugin
-	attr_accessor :plugin_name
-	@registered_plugins = []
+        attr_accessor :plugin_name
+        @registered_plugins = []
 
-	class << self
-		attr_reader :registered_plugins
-		private :new
-	end
+        class << self
+                attr_reader :registered_plugins
+                private :new
+        end
 
-	def self.define(name, &block)
-		p = new
-		p.instance_eval(&block)
-		p.plugin_name = name
-		Plugin.registered_plugins << p
-	end
+        def self.define(name, &block)
+                p = new
+                p.instance_eval(&block)
+                p.plugin_name = name
+                Plugin.registered_plugins << p
+        end
 
 end
 
 
 Plugin.define "first" do
-	def generate(n)
-		n.format("%f")
-	end
+        def generate(n)
+                n.format("%f")
+        end
 end
 
 Plugin.define "firstlast" do
-	def generate(n)
-		n.format_anna("annakey")
-	end
+        def generate(n)
+                n.format_anna("annakey")
+        end
 end
 
 
 Plugin.define "first.last" do
-	def generate(n)
-		#n.format("%f.%l")
-		n.format_anna("anna.key")
-	end
+        def generate(n)
+                #n.format("%f.%l")
+                n.format_anna("anna.key")
+        end
+end
+
+Plugin.define "first_last" do
+        def generate(n)
+                #n.format("%f.%l")
+                n.format_anna("anna_key")
+        end
+end
+
+Plugin.define "first-last" do
+        def generate(n)
+                #n.format("%f.%l")
+                n.format_anna("anna-key")
+        end
 end
 
 Plugin.define "firstlast[8]" do
-	def generate(n)
-		# fix this. truncated to 8
-		(n.firstname + n.lastname).to_s[0..7] unless n.firstname.nil? or n.lastname.nil?		
-	end
+        def generate(n)
+                # fix this. truncated to 8
+                (n.firstname + n.lastname).to_s[0..7] unless n.firstname.nil? or n.lastname.nil?
+        end
 end
 
 Plugin.define "first[4]last[4]" do
-	def generate(n)		
-		(n.firstname.to_s[0..3]) + (n.lastname.to_s[0..3]) unless n.firstname.nil? or n.lastname.nil?
-	end
+        def generate(n)
+                (n.firstname.to_s[0..3]) + (n.lastname.to_s[0..3]) unless n.firstname.nil? or n.lastname.nil?
+        end
+end
+
+Plugin.define "first[1]last[7]" do
+        def generate(n)
+                (n.firstname.to_s[0..0]) + (n.lastname.to_s[0..6]) unless n.firstname.nil? or n.lastname.nil?
+        end
+end
+
+Plugin.define "first[7]last[1]" do
+        def generate(n)
+                (n.firstname.to_s[0..6]) + (n.lastname.to_s[0..0]) unless n.firstname.nil? or n.lastname.nil?
+        end
+end
+
+Plugin.define "first[5]last[5]" do
+        def generate(n)
+                (n.firstname.to_s[0..4]) + (n.lastname.to_s[0..4]) unless n.firstname.nil? or n.lastname.nil?
+        end
 end
 
 Plugin.define "firstl" do
-	def generate(n)
-		#n.format("%f.%l")
-		n.format_anna("annak")
-	end
+        def generate(n)
+                #n.format("%f.%l")
+                n.format_anna("annak")
+        end
 end
 
 Plugin.define "f.last" do
-	def generate(n)
-		#n.format("%i.F.%L")
-		n.format_anna("a.key")
-	end
+        def generate(n)
+                #n.format("%i.F.%L")
+                n.format_anna("a.key")
+        end
+end
+
+Plugin.define "f-last" do
+        def generate(n)
+                #n.format("%i.F.%L")
+                n.format_anna("a-key")
+        end
+end
+
+Plugin.define "f_last" do
+        def generate(n)
+                #n.format("%i.F.%L")
+                n.format_anna("a_key")
+        end
 end
 
 Plugin.define "flast" do
-	def generate(n)
-		#n.format("%i.F.%L")
-		n.format_anna("akey")
-	end
+        def generate(n)
+                #n.format("%i.F.%L")
+                n.format_anna("akey")
+        end
 end
 
 Plugin.define "lfirst" do
-	def generate(n)
-		n.format_anna("kanna")
-	end
+        def generate(n)
+                n.format_anna("kanna")
+        end
 end
 
 Plugin.define "l.first" do
-	def generate(n)
-		n.format_anna("k.anna")
-	end
+        def generate(n)
+                n.format_anna("k.anna")
+        end
+end
+
+Plugin.define "l-first" do
+        def generate(n)
+                n.format_anna("k-anna")
+        end
+end
+
+Plugin.define "l_first" do
+        def generate(n)
+                n.format_anna("k_anna")
+        end
 end
 
 Plugin.define "lastf" do
-	def generate(n)
-		n.format_anna("keya")
-	end
+        def generate(n)
+                n.format_anna("keya")
+        end
 end
 
 
 Plugin.define "last" do
-	def generate(n)
-		n.format_anna("key")
-	end
+        def generate(n)
+                n.format_anna("key")
+        end
 end
 
 Plugin.define "last.f" do
-	def generate(n)
-		n.format_anna("key.a")
-	end
+        def generate(n)
+                n.format_anna("key.a")
+        end
+end
+
+Plugin.define "last-f" do
+        def generate(n)
+                n.format_anna("key-a")
+        end
+end
+
+Plugin.define "last_f" do
+        def generate(n)
+                n.format_anna("key_a")
+        end
 end
 
 Plugin.define "last.first" do
-	def generate(n)
-		n.format_anna("key.anna")
-	end
+        def generate(n)
+                n.format_anna("key.anna")
+        end
+end
+
+Plugin.define "last-first" do
+        def generate(n)
+                n.format_anna("key-anna")
+        end
+end
+
+Plugin.define "last_first" do
+        def generate(n)
+                n.format_anna("key_anna")
+        end
 end
 
 Plugin.define "FLast" do
-	def generate(n)
-		#n.firstinitial + n.lastname
-		n.format_anna("AKey")
-	end
+        def generate(n)
+                n.firstinitial + n.lastname
+                #n.format_anna("AKey")
+        end
 end
 
 Plugin.define "first1" do
-	def generate(n)
-		n.format_anna("anna%D")
-	end
+        def generate(n)
+                n.format_anna("anna%D")
+        end
 end
 
 Plugin.define "fl" do
-	def generate(n)
-		#n.firstinitial + n.lastname
-		n.format_anna("ak")
-	end
+        def generate(n)
+                #n.firstinitial + n.lastname
+                n.format_anna("ak")
+        end
 end
 
 Plugin.define "fmlast" do
-	def generate(n)
-		#n.firstinitial + n.lastname
-		n.format_anna("abkey")
-	end
+        def generate(n)
+                #n.firstinitial + n.lastname
+                n.format_anna("abkey")
+        end
 end
 
 
 Plugin.define "firstmiddlelast" do
-	def generate(n)
-		#n.firstinitial + n.lastname
-		n.format_anna("annaboomkey")
-	end
+        def generate(n)
+                #n.firstinitial + n.lastname
+                n.format_anna("annaboomkey")
+        end
 end
 
 Plugin.define "fml" do
-	def generate(n)
-		#n.firstinitial + n.lastname
-		n.format_anna("abk")
-	end
+        def generate(n)
+                #n.firstinitial + n.lastname
+                n.format_anna("abk")
+        end
 end
 
 #Plugin.define "canterbury-uni" do
-#	def generate(n)
-#		#n.firstinitial + n.lastname
-#		n.format_anna("abk%DD")
-#	end
+#       def generate(n)
+#               #n.firstinitial + n.lastname
+#               n.format_anna("abk%DD")
+#       end
 #end
 
 Plugin.define "FL" do
-	def generate(n)
-		#n.firstinitial + n.lastname
-		n.format_anna("AK")
-	end
+        def generate(n)
+                #n.firstinitial + n.lastname
+                n.format_anna("AK")
+        end
 end
 
 
 Plugin.define "FirstLast" do
-	def generate(n)
-		n.format_anna("%F%L")
-	end
+        def generate(n)
+                n.format_anna("%F%L")
+        end
 end
 
 Plugin.define "First.Last" do
-	def generate(n)
-		n.format_anna("%F.%L")
-	end
+        def generate(n)
+                n.format_anna("%F.%L")
+        end
+end
+
+Plugin.define "First_Last" do
+        def generate(n)
+                n.format_anna("%F_%L")
+        end
+end
+
+Plugin.define "First-Last" do
+        def generate(n)
+                n.format_anna("%F-%L")
+        end
 end
 
 Plugin.define "Last" do
-	def generate(n)
-		n.format_anna("Key")
-	end
+        def generate(n)
+                n.format_anna("Key")
+        end
 end
 =begin
 
 Plugin.define "FML" do
-	def generate(n)
-		# this is the sort of code I want to avoid
-		s=""
-		s+=n.firstinitial.downcase
+        def generate(n)
+                # this is the sort of code I want to avoid
+                s=""
+                s+=n.firstinitial.downcase
 
-		if n.middleinitial
-			s+=n.middleinitial
-		else
-			s+="~"
-		end
-		s+=n.lastinitial.downcase
+                if n.middleinitial
+                        s+=n.middleinitial
+                else
+                        s+="~"
+                end
+                s+=n.lastinitial.downcase
 
-		if s=~/~/
-			names=('a'..'z').map {|letter| s.sub('~',letter) }
-			s=names
-		end
-		s
-		n.format_anna("ABK")
-	end
+                if s=~/~/
+                        names=('a'..'z').map {|letter| s.sub('~',letter) }
+                        s=names
+                end
+                s
+                n.format_anna("ABK")
+        end
 end
 =end
-
